c18a9c1b9eabc6e23e75497645adf66c199f408c
tianjian <1731857742@qq.com>
2017-09-13 15:51:53 +0800

    静态文件bug修复

--- netty-http-core/src/main/java/tianjian/http/spring/test/BeanFactoryPostProcessorImpl.java
+++ netty-http-core/src/main/java/tianjian/http/spring/test/BeanFactoryPostProcessorImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.stereotype.Service;
 import tianjian.http.controller.oauth2.impl.DefaultTokenGenerator;
 
 /**
